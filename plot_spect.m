56a7e0002444b53803bf34b7572b996ca26b73e5
Ryan Tse <rytse@protonmail.com>
2019-02-25 19:52:23 -0500

    End of day commit 2019-02-25

--- plot_spect.m
+++ plot_spect.m
@@ -1,25 +1,35 @@
-ELAPSED_TIME = 1;
+ELAPSED_TIME = 5;
+SAMP_RATE = 50e6;
+START_F = 3.395e6;
+STOP_F = 3.874e6 * 8;
+REP_T = 6;
 
-[fn, pn] = uigetfile('*.iq');
-% fullfile(pn, fn);
-fid = fopen(fullfile(pn, fn));
-% s = fread(fid,inf,'int16');
-% s = fread(fid, 50e6 * 6, 'int16');
-s = fread(fid, 50e6 * ELAPSED_TIME, 'int16');
+% Generate simualated chirp
+ts = 0 : 1 / SAMP_RATE : ELAPSED_TIME;
+ts = ts(1 : length(ts) - 1);
+ch = chirp(ts, START_F, REP_T, STOP_F)';
+
+% Load noise from recording
+% [fn, pn] = uigetfile('*.iq');
+% fid = fopen(fullfile(pn, fn));
+fid = fopen('./data/own/IQREC-02-03-19-13h38m31s479.iq');
+s = fread(fid, SAMP_RATE * ELAPSED_TIME * 2, 'int16');
 fclose(fid);
-
 s_c = complex(s(1:2:end),s(2:2:end));
+
+% Add them
+rep = s_c + ch * 1000;
+
+% Plot them!
 win = nuttallwin(65536*4*4*4);
 f = zeros(length(win),floor(length(s_c)/length(win)/2));
 jj = 1;
-
 for ii = 1 : length(win) / 2 : length(s_c) - length(win)
-  f(:, jj) = mag2db(abs(fftshift(fft(s_c(ii : ii + length(win) - 1) .* win))));
+  f(:, jj) = mag2db(abs(fftshift(fft(rep(ii : ii + length(win) - 1) .* win))));
   jj = jj + 1;
 end
-
 [i,j] = size(f);
-gr = imagesc(linspace(0, ELAPSED_TIME, length(f)),linspace(0, 40, i), f)
+gr = imagesc(linspace(0, ELAPSED_TIME, length(f)),linspace(0, length(rep) / SAMP_RATE * ELAPSED_TIME, i), f)
 set(gca,'YDir','normal')
 title(fn);
 colormap(hsv)
